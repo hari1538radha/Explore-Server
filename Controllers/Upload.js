8d3120e960872ce34e66b9bc5e25b07e242d4998
hari1538radha <hari.r2020ece@sece.ac.in>
2023-02-16 05:52:32 +0530

    fix : sending user's firstname

--- Controllers/Upload.js
+++ Controllers/Upload.js
@@ -1,22 +1,19 @@
-
 import { uploadModel } from "../Schema/UploadSchema.js";
 
 export const uploadData = (req, res) => {
-  
-      // const addImage = new uploadModel(req.body);
-      const addImage = new uploadModel({
-        placeName: req.body.placeName,
-        placeTag: req.body.placeTag,
-        placeDescription: req.body.placeDescription,
-        placeImage: req.body.placeImage,
-        cardId : Math.floor(1000 + Math.random() * 9000)
-      });
-      addImage.save((err, data) => {
-        if (err) {
-          res.send(err);
-        }
-        res.status(200).send({
-          message: "Posted sucessfully!!!",
-        });
-      });
+  const addImage = new uploadModel({
+    placeName: req.body.placeName,
+    placeTag: req.body.placeTag,
+    placeDescription: req.body.placeDescription,
+    placeImage: req.body.placeImage,
+    cardId: Math.floor(1000 + Math.random() * 9000),
+  });
+  addImage.save((err, data) => {
+    if (err) {
+      res.send(err);
     }
+    res.status(200).send({
+      message: "Posted successfully!!!",
+    });
+  });
+};

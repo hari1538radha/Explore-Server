7103071f1977ac94bd9b78d8f7dbc03fb955c024
hari1538radha <hari.r2020ece@sece.ac.in>
2023-02-18 09:42:51 +0530

    fix :messages give to response block

--- Controllers/searchdetail.js
+++ Controllers/searchdetail.js
@@ -5,12 +5,14 @@ export const searchedDetails = (req, res) => {
   const details = uploadModel.findOne({ _id: body.id }, (err, data) => {
     if (err) {
       res.send({
-        status: 400,
+        status: 401,
+        message:"error",
         err,
       });
     } else {
       res.send({
         status: 200,
+        message:"success",
         data,
       });
     }
